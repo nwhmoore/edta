fc2dc95b731097edd4734f8632a5e96dce13cf65
nwhmoore <bg85@cdc.gov>
2025-10-06 14:39:56 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use std::fs::{File, write};
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let data: Vec<String> = make_clean_fasta_data("rosalind_edta.txt"); //read in fasta data
+    //println!("{:?}",data); //debug
+
+    // get two strings to align
+    let x: &String = &data[0];
+    let y: &String = &data[1];
+
+    // Needleman-Wunsch algorithm:
+    let (z,w,distance) = needleman_wunsch(x, y);
+    let ans: String = format!("{}\n{}\n{}",distance,z,w);
+    println!("{}",ans);
+    write("ans.txt",ans).expect("Should write to file");
+}
+
+fn needleman_wunsch(x: &String, y: &String) -> (String, String, i32) {
+    // construct matrix
+    let mut mat: Vec<Vec<i32>> = vec![vec![0i32;x.len()+1];y.len()+1];
+
+    //preallocate first row
+    for i in 0..x.len()+1 {
+        mat[0][i] = -(i as i32);
+    }
+    //preallocate first column
+    for j in 0..y.len()+1 {
+        mat[j][0] = -(j as i32);
+    }
+
+    //iterate through matrix
+    //current cell is i+1, j+1
+    for j in 0..y.len() {
+        for i in 0..x.len(){
+            let score:i32;
+            // if it's a match
+            if y[j..j+1] == x[i..i+1] {
+                score = 0;
+            } else {
+                score = -1;
+            }
+
+            // score based on adjacent cells
+            let top: i32 = mat[j][i+1] + score;
+            let left: i32 = mat[j+1][i] + score;
+            let diag: i32 = mat[j][i] + score;
+            let adj_cells: [i32; 3] = [top,left,diag];
+
+            // find max score
+            let max_val: Option<&i32> = adj_cells.iter().max();
+            match max_val {
+                Some(cell) => mat[j+1][i+1] = *cell, //assign cell score
+                None => ()
+            }
+        }
+    }
+
+    let edit_distance: i32 = -mat[y.len()][x.len()];
+    println!("{:?}",mat);
+    //find aligment strings
+    //start at bottom-right of matrix, find path to top left
+    let mut z: String = String::new();
+    let mut w: String = String::new();
+
+    // CHOOSE PATHS
+    // NEED WHILE TO DO (0,0) CELL
+    let mut i: usize = x.len()-1;
+    let mut j: usize = y.len()-1;
+    while !(i == 0 && j == 0) {
+        //current cell is [j+1][i+1]
+        let top: i32 = mat[j][i+1];
+        let left: i32 = mat[j+1][i];
+        let diag: i32 = mat[j][i];
+
+        if top > left && top > diag { //top
+            j -= 1;
+            w.push('-');
+        } else if left > top && left > diag { //left
+            i -= 1;
+            z.push('-');
+        } else { //diag
+            z.push_str(&x[i..i+1]);
+            w.push_str(&y[j..j+1]);
+            i -= 1;
+            j -= 1;
+        }
+
+    }
+    return(z,w,edit_distance);
+}
+
+
+fn make_clean_fasta_data(filepath:&str) -> Vec<String> {
+    let mut data:Vec<String> = vec![String::new();0]; // final output string vector
+    let mut data_line:String = String::new(); // current strand
+
+    // File hosts.txt must exist in the current path
+    if let Ok(lines) = read_lines(filepath) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.map_while(Result::ok) {
+            //println!("{}",line); //debug
+            let tag = &line[..1]; //FASTA ID line
+            if tag == ">" { // if ID line
+                data.push(data_line); // save previous strand //MAKES EMPTY FIRST SLOT
+                data_line = String::new(); // start new strand
+            } else {
+                data_line.push_str(&line); //push file line to current strand
+            }
+        }
+        data.push(data_line); // push final string to strand
+    } else {
+        println!("Can't read file!");
+    }
+    data.remove(0); // remove first empty slot
+    //println!("{:?}",data); //debug
+    return data;
+}
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
